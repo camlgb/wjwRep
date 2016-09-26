4d1bc65855025297c3f0d199b1d0fdc06387bf4f
ff <1025019486@qq.com>
2016-09-26 15:15:30 +0800

    as
    sa

---
+++ MainInitialicer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace SocialPoint.Examples.MVC
+{
+    public class MainInitialicer : MonoBehaviour 
+    {
+        PlayerController Controller;
+
+        void Start()
+        {
+            Controller = new PlayerController();
+
+            Controller.ShowView();
+        }
+    }
+}
+//jiajiajaijia
+//diercigaibiansdrgerghrehrtjtykjyuluil
